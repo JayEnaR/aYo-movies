94539376bd95840f7d9cdcb788327d631415266c
JayEnaR <ervinvdmerwe@yahoo.com>
2023-04-14 11:51:21 +0200

    search audiovisuals by type

--- aYo-movies/src/app/components/search-bar/search-bar.component.ts
+++ aYo-movies/src/app/components/search-bar/search-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AbstractControl, AsyncValidatorFn, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { debounceTime, distinctUntilChanged, first, map, switchMap } from 'rxjs';
+import { debounceTime, distinctUntilChanged, first, map, switchMap, tap } from 'rxjs';
+import { QueryTypeEnum } from 'src/app/enums/query-type.enum';
 import { ISearchResult } from 'src/app/models/ISearchResult';
 import { MovieSearchService } from 'src/app/services/movie-search.service';
 
@@ -13,6 +14,7 @@ export class SearchBarComponent implements OnInit {
 
   searchForm: FormGroup;
   isSearching: boolean = false;
+  queryType: QueryTypeEnum = QueryTypeEnum.movie;
 
   constructor(private _formBuilder: FormBuilder,
     private _movieSearchService: MovieSearchService) {
@@ -23,6 +25,8 @@ export class SearchBarComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this._movieSearchService.cinema$.subscribe(a => console.log(a)
+    )
   }
 
   buildForm(): FormGroup {
@@ -31,14 +35,35 @@ export class SearchBarComponent implements OnInit {
     });
   }
 
+  initQuery(name: string): string {
+    switch(this.queryType){
+      case QueryTypeEnum.movie : {
+        name = `type=movie&t=${name}`;
+        break;
+      }
+      case QueryTypeEnum.series : {
+        name = `type=series&t=${name}`;
+        break;
+      }
+      case QueryTypeEnum.episode : {
+        name = `type=episode&t=${name}`;
+        break;
+      }
+    }
+    return name;
+  }
+
+  clearSearch(): void {
+
+  }
+
   searchBarValidator(): AsyncValidatorFn {
     return (control: AbstractControl) => {
       return control.valueChanges.pipe(
         debounceTime(1000),
         distinctUntilChanged(),
-        switchMap(phrase => this._movieSearchService.retrieveCinema(phrase)),
-        map((res: ISearchResult) => res ? { noResult: true } : null),
-        first()
+        switchMap(phrase => this._movieSearchService.retrieveCinema(this.initQuery(phrase))),
+        map((res: ISearchResult) => res ? { noResult: true } : null)
       );
     }
   }
