aa7ee08da003411c99f2391caf6f65f935f7f0ec
JayEnaR <ervinvdmerwe@yahoo.com>
2023-04-14 11:17:24 +0200

    get/set result from movie search service

--- aYo-movies/src/app/services/movie-search.service.ts
+++ aYo-movies/src/app/services/movie-search.service.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@angular/core';
 import { BaseApiService } from './base-api.service';
 import { ConfigService } from './config.service';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { MochApiService } from './moch-api.service';
+import { ISearchResult } from '../models/ISearchResult';
 
 /**
  *  A service for retrieving movies
@@ -13,13 +14,25 @@ import { MochApiService } from './moch-api.service';
 })
 export class MovieSearchService extends BaseApiService {
 
+  private cinemaSrc: Subject<ISearchResult> = new Subject<ISearchResult>();
+
+  get cinema$(): Observable<ISearchResult> {
+    return this.cinemaSrc.asObservable();
+  }
+
+  set cinema(item: ISearchResult){
+    this.cinemaSrc.next(item);
+  }
+
   constructor(private _httpClient: HttpClient,
     private configService: ConfigService,
     private _mochApiService: MochApiService) {
     super(_httpClient, configService.apiBaseUrl);
   }
 
-  getMovie(param: string): Observable<any> {
+  // Retrieve audiovisual media from api
+  retrieveCinema(param: string): Observable<ISearchResult> {
+    this.cinemaSrc.next({} as ISearchResult);
     param = `?apikey=${this._mochApiService.apiKey}/${param}`;
     return super.Get(param);
   }
