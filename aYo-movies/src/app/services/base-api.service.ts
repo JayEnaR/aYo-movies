60824da83d0f39d18281fce67de0183787764312
JayEnaR <ervinvdmerwe@yahoo.com>
2023-04-15 09:13:13 +0200

    use postman to prevent daily api call quota

--- aYo-movies/src/app/services/base-api.service.ts
+++ aYo-movies/src/app/services/base-api.service.ts
@@ -25,6 +25,14 @@ export class BaseApiService {
       );
   }
 
+  // Used Postman to prevent hitting the daily quota for api calls
+  protected GetMochData<T>(param: string): Observable<T> {
+    return this.httpClient.get<T>("https://61eb7e51-31b0-4acb-840e-975cf4fe488e.mock.pstmn.io")
+      .pipe(
+        catchError(e => this.handleError(e))
+      );
+  }
+
   private handleError(e: HttpErrorResponse): Observable<any> {
     // Could display some notification to the user here
     return of(e);
