90500c002ef6f7fc423a651cfc2db451bd1edb97
JayEnaR <ervinvdmerwe@yahoo.com>
2023-04-16 15:03:58 +0200

    search bar test

--- aYo-movies/src/app/components/search-bar/search-bar.component.spec.ts
+++ aYo-movies/src/app/components/search-bar/search-bar.component.spec.ts
@@ -1,14 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchBarComponent } from './search-bar.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AudioVisualSearchService } from 'src/app/services/audio-visual-search.service';
+import { SnackbarService } from 'src/app/services/snackbar.service';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { QueryTypeEnum } from 'src/app/enums/query-type.enum';
 
 describe('SearchBarComponent', () => {
   let component: SearchBarComponent;
   let fixture: ComponentFixture<SearchBarComponent>;
 
+  class MockServiceDependencyStub {
+    showSnackBar() { }
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SearchBarComponent ]
+      imports:[       
+        ReactiveFormsModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        FormsModule],
+      declarations: [ SearchBarComponent ],
+      providers: [ AudioVisualSearchService, { provide: SnackbarService, useClass: MockServiceDependencyStub }]
     })
     .compileComponents();
 
@@ -20,4 +36,19 @@ describe('SearchBarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should clear search', () => {
+    component.clearSearch();
+  });
+
+  it('must filter by movie type', () => {
+    const type = QueryTypeEnum.movie;
+    component.onChipChange(type);
+  });
+
+  it('must validate on search input change', () => {
+    const type = QueryTypeEnum.movie;
+    component.searchBarValidator();
+  });
+
 });
