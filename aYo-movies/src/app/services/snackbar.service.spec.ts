c847d5bcf927ffe65b33760d845c30ea08ac3294
JayEnaR <ervinvdmerwe@yahoo.com>
2023-04-16 12:25:51 +0200

    snackbar service test

--- aYo-movies/src/app/services/snackbar.service.spec.ts
+++ aYo-movies/src/app/services/snackbar.service.spec.ts
@@ -1,16 +1,31 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SnackbarService } from './snackbar.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { SnackbarTypeEnum } from '../enums/snackbar-type.enum';
+
+class MockServiceDependencyStub {
+  open(){}
+}
 
 describe('SnackbarService', () => {
   let service: SnackbarService;
+  // let mockValidator: jasmine.SpyObj<MatSnackBar>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [SnackbarService, { provide: MatSnackBar, useClass: MockServiceDependencyStub }]
+    });
     service = TestBed.inject(SnackbarService);
+
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should show a snackbar', () => {
+    service.showSnackBar("Snackbar works", SnackbarTypeEnum.success, 5000);
+  });
+
 });
