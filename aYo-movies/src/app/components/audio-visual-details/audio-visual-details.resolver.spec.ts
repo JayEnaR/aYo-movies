e3b97bafc125c56c6fa2a53d7720273d4af64e48
JayEnaR <ervinvdmerwe@yahoo.com>
2023-04-16 15:42:13 +0200

    resolver test

--- aYo-movies/src/app/components/audio-visual-details/audio-visual-details.resolver.spec.ts
+++ aYo-movies/src/app/components/audio-visual-details/audio-visual-details.resolver.spec.ts
@@ -1,16 +1,28 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AudioVisualDetailsResolver } from './audio-visual-details.resolver';
+import { HttpClientModule } from '@angular/common/http';
+import { SnackbarService } from 'src/app/services/snackbar.service';
+import { ISearchResult } from 'src/app/models/ISearchResult';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+class MockServiceDependencyStub {
+  showSnackBar(){}
+}
 
 describe('AudioVisualDetailsResolver', () => {
   let resolver: AudioVisualDetailsResolver;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [AudioVisualDetailsResolver, { provide: SnackbarService, useClass: MockServiceDependencyStub }]
+    });
     resolver = TestBed.inject(AudioVisualDetailsResolver);
   });
 
   it('should be created', () => {
     expect(resolver).toBeTruthy();
   });
+
 });
