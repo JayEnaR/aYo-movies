82dd6da91d127c696755f5f940c124915f9b2071
JayEnaR <ervinvdmerwe@yahoo.com>
2023-04-16 12:58:25 +0200

    indexed db test

--- aYo-movies/src/app/services/indexed-db.service.spec.ts
+++ aYo-movies/src/app/services/indexed-db.service.spec.ts
@@ -1,16 +1,35 @@
 import { TestBed } from '@angular/core/testing';
 
 import { IndexedDbService } from './indexed-db.service';
+import { SnackbarService } from './snackbar.service';
+import { ISearchResult } from '../models/ISearchResult';
+
+class MockServiceDependencyStub {
+  showSnackBar(){}
+}
 
 describe('IndexedDbService', () => {
   let service: IndexedDbService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [IndexedDbService, { provide: SnackbarService, useClass: MockServiceDependencyStub }]
+    });
     service = TestBed.inject(IndexedDbService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should add to watchlist', () => {
+    const val: ISearchResult = { imdbID: "123", Title: "Test" } as ISearchResult;
+    service.addAudioVisual(val);
+  });
+
+  it('should remove from watchlist', () => {
+    const val: ISearchResult = { imdbID: "123", Title: "Test" } as ISearchResult;
+    service.deleteAudioVisual(val);
+  });
+
 });
